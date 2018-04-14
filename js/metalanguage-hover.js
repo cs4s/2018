004f4ef71b4d6ae7f6a50bef4010d74da7edd199
Daniel Hickmott <daniel.hickmott@gmail.com>
2018-04-14 23:17:33 +1000

    Changes hover scripts to show preview of definition

--- js/metalanguage-hover.js
+++ js/metalanguage-hover.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
 
 	// Get all the metalanguage terms on the page and create empty template divs
 	$.each(metalanguage_terms, function(index, term) {
+
 		var term_id = term.innerText.toLowerCase().replace(/ /g, "-");
+		console.log(term);
+		$(this).prop("href", "../glossary/index.html#" + term_id);
+
 		term.id = "metalanguage-" + term_id 
 		term_ids.push(term_id);
 
@@ -18,8 +22,9 @@ $(document).ready(function() {
 	temp_div.load("../glossary/index.html", function() {
 		$.each(term_ids, function(index, term_id) {
 			var glossary_term_elements = $("#" + term_id, temp_div).nextUntil('h3');
+			var glossary_term_preview = glossary_term_elements[0];
 			var term_template_div = $("#" + term_id + "-template");
-			term_template_div.html(glossary_term_elements);
+			term_template_div.html(glossary_term_preview);
 			tippy("#metalanguage-" + term_id, {
 				html: "#" + term_id + "-template",
 				size: "large",
